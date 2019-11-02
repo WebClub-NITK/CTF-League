b3195589aeb64fad85b6528d707b256f3f7a09a6
adwaith-os <adwi@adwi.nitk.ac.in>
2019-11-02 08:39:07 +0530

    Changed pwnme.c to make it suitable for nc'ing from their terminal

--- intro-session/pwn/pwnme.c
+++ intro-session/pwn/pwnme.c
@@ -6,26 +6,26 @@
 void sigsegv_handler(int sig)
 {
 	printf("You got a shell!!\n");
+	fflush(stdout);
+
 	system("/bin/sh");
 }
 
-void vuln(char *input)
+void vuln()
 {
 	char buf[16];
-	strcpy(buf, input);
+	printf("Enter any string you want!");
+	fflush(stdout);
+
+	gets(buf);
+	fflush(stdin);
 }
 
 int main(int argc, char **argv)
 {
 	signal(SIGSEGV, sigsegv_handler);
 
-	if(argc != 2)
-	{
-		printf("Usage: %s your_string\n", argv[0]);
-		return -1;
-	}
-
-	vuln(argv[1]);
+	vuln();
 
 	return 0;
 }
